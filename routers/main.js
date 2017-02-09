771cd53802f652b1de980d561f2c7230cc53f0b2
cx <chenxi08@zuche.com>
2017-02-09 11:33:20 +0800

    update 2

--- routers/main.js
+++ routers/main.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function ( req , res , next ) {
-	//console.log(req.userInfo.username)
+	//console.log(req.userInfo.username);
     res.render('main/index',{
     	userInfo:req.userInfo.username
     });
